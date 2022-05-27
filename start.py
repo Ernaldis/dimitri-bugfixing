35a64f1bd04824aeb6f2f0621b24d1293a296131
ernaldis <ernaldis@protonmail.com>
2022-05-27 16:23:21 -0500

    fimplify start and add ports and workercount to compose

--- start.py
+++ start.py
@@ -5,39 +5,9 @@ dj.config['safemode']=False
 schema = dj.Schema("ernaldis_test")
 
 @schema
-class Website(dj.Lookup):
-    definition = """
-    website : int
-    ---
-    url : varchar(1000)
-    """
-    contents = [
-        (100, "Nobody expects the spammish repetition"),
-        (200, "Nobody expects the spammish repetition"),
-        (300, "Nobody expects the spammish repetition"),
-        (400, "Nobody expects the spammish repetition"),
-        (500, "Nobody expects the spammish repetition"),
-        (600, "Nobody expects the spammish repetition"),
-        (700, "Nobody expects the spammish repetition"),
-        (800, "Nobody expects the spammish repetition"),
-        (900, "Nobody expects the spammish repetition"),
-        (1000, "Nobody expects the spammish repetition"),
-        (1100, "Nobody expects the spammish repetition"),
-        (1200, "Nobody expects the spammish repetition"),
-        (1300, "Nobody expects the spammish repetition"),
-        (1400, "Nobody expects the spammish repetition"),
-        (1500, "Nobody expects the spammish repetition")
-    ]
-
-@schema
 class Stats(dj.Computed):
     definition = """
     -> Website
     """
-    def make(self, key):
-        print("Running make with key=", key)
-        time.sleep(key)
-        url = (Website & key).fetch1('url')
-        self.insert1(key)
 
 Stats.delete()
